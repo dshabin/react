2f2178fd59449a8d6b832a67111a0d071d145243
Shabin <d.shabin@gmail.com>
2017-05-31 20:35:01 +0400

    Random number render

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(p){
+    super(p);
+    this.state = {names : ['A']};
+  }
+
+  componentDidMount(){
+    const choices = ['a','b','c','d'];
+    setInterval(() => {
+      const names = [...this.state.names];
+      names.push(choices[Math.floor(Math.random() * 4)]);
+      this.setState({names:names});
+    },3 * 1000);
+  }
+
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    const names = this.state.names.map(elem => {
+      return (<p>{elem}</p>);
+    })
+    return (<div>{names}</div>);
   }
 }
 
